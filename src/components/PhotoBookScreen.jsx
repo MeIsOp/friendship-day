f99a3190e7f5db33ec16b4a78435f71745388877
Piyush Agarwal <djpiyush45@gmail.com>
2025-08-03 15:07:59 +0530

    Added new memory photo 5

--- src/components/PhotoBookScreen.jsx
+++ src/components/PhotoBookScreen.jsx
@@ -8,6 +8,7 @@ const photos = [
   "/images/ghibli 2.png",
   "/images/ghibli 3.png",
   "/images/ghibli 4.jpg",
+  "/images/ghibli 5.jpg",
 ]
 
 export default function PhotoBookScreen({ onNext }) {
